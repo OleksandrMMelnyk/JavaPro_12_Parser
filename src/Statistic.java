968412237061fb3e7bff9be76d834d9d5a9de158
OleksandrMMelnyk <OleksandrMMelnyk@gmail.com>
2024-05-07 15:04:09 +0300

    init commit

---
+++ src/Statistic.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class Statistic {
+
+    private final List<String> top10;
+    private final int uniqWords;
+
+    public Statistic(List<String> top10, int uniqWords) {
+        this.top10 = top10;
+        this.uniqWords = uniqWords;
+    }
+
+    @Override
+    public String toString() {
+        return "Statistic{" +
+                "top10=" + top10 +
+                ", uniqWords=" + uniqWords +
+                '}';
+    }
+}
